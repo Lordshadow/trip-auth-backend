59c1fbf54e80be6534c074a86a1dc7b6bed6917d
Lordshadow <kaadithiya@gmail.com>
2025-04-23 16:46:32 +0530

    minorcahnge

--- models/Profile.js
+++ models/Profile.js
@@ -4,7 +4,7 @@ const profileSchema = new mongoose.Schema({
     firebaseUID: { type: String, required: true, unique: true },
     email: { type: String, required: true },
     phoneNumber: { type: String },
-   // name: { type: String }, // Optional, can be added later
+     name: { type: String }, // Optional, can be added later
   });
   
   module.exports = mongoose.model('users', profileSchema);

cb34e1f3f7c52f650c1fe76dd7c934fc73ca543c
Lordshadow <kaadithiya@gmail.com>
2025-04-23 11:18:35 +0530

    firebasekey added back

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = require("./firebaseServiceKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
